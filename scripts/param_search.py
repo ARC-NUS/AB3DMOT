71ce170049b25908eba168b0157bbaadc13cc616
snail <mpechon@nus.edu.sg>
2019-10-29 13:57:08 +0800

    corrected q

--- scripts/param_search.py
+++ scripts/param_search.py
@@ -147,9 +147,9 @@ def parallel_qv(pixor_json_name,pixor_stats_json, fused_pose_json, labels_json_p
       for ha in np.arange(0.05,0.9,0.05):
         for qv_i in np.arange(10):
           q_v = 10.**(qv_i-5)
-          Q = np.zeros((STATE_SIZE, STATE_SIZE))
-          Q[0,0]=delta_t**3*q_v/3.
-          Q[1,1]=delta_t**3*q_v/3.
+          Q = np.eye(STATE_SIZE)*delta_t
+          Q[0,0]=delta_t+delta_t**3*q_v/3.
+          Q[1,1]=delta_t+delta_t**3*q_v/3.
           Q[0,7]=delta_t**2*q_v/2.
           Q[1,8]=delta_t**2*q_v/2.
           Q[7,0]=delta_t**2*q_v/2.
@@ -171,11 +171,11 @@ def parallel_qv(pixor_json_name,pixor_stats_json, fused_pose_json, labels_json_p
           if MOTA >= best_MOTA:
             best_MOTA = MOTA
             best_MOTA_p = [min_hits, max_age, ha, qv_i, MOTA, MOTP]
-            print "best MOTA params", MOTA, best_params
+            print "best MOTA params", MOTA, best_MOTA_p
           if MOTP <= best_MOTP:
             best_MOTP = MOTP
             best_MOTP_p = [min_hits, max_age, ha, qv_i, MOTA, MOTP]
-            print "best MOTP params", MOTP, best_params
+            print "best MOTP params", MOTP, best_MOTP_p
   print "bestest:", score, best_params
   print "best MOTA", best_MOTA, best_MOTA_p
   print "best MOTP", best_MOTP, best_MOTP_p
