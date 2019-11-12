ad00d2da25c123b66eecd4a17f261bb9c918b26d
snail <mpechon@nus.edu.sg>
2019-11-12 22:32:23 +0800

    added script for predictions from labels

---
+++ scripts/predictor_wt_labels.py
@@ -0,0 +1,12 @@
+#/usr/bin/env python
+
+import yl_utils as yl
+
+
+def get_pred_json(label_json,output_pred_json):
+  
+
+
+if __name__ == '__main__':
+  label_json = "/media/yl/downloads/raw_data/CETRAN_ST-cloudy-day_2019-08-27-22-47-10/11_sep/log_low/set_8/labels.old/Set_8_annotations.json"
+  output_pred_json ="/media/yl/downloads/raw_data/CETRAN_ST-cloudy-day_2019-08-27-22-47-10/11_sep/log_low/set_8/prediction.json"
