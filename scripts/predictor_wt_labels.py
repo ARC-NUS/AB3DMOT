2ef2afbacbc113e56ae2d2708ac414b3e2128323
snail <mpechon@nus.edu.sg>
2019-11-12 22:42:38 +0800

    prediction code in proegres

--- scripts/predictor_wt_labels.py
+++ scripts/predictor_wt_labels.py
@@ -4,9 +4,19 @@ import yl_utils as yl
 
 
 def get_pred_json(label_json,output_pred_json):
-  
+  with open(label_json) as json_file:
+    with open(fused_pose_json) as fp_json:
+      labels_data = json.load(json_file, encoding="utf-8")
+      fp_data = json.load(fp_json, encoding="utf-8") # TODO use fp to do tf
+      
+      for index, labels in enumerate(labels_data): # for each pcd/timestep labelled
+        labels['name']
+      
+      
 
 
 if __name__ == '__main__':
-  label_json = "/media/yl/downloads/raw_data/CETRAN_ST-cloudy-day_2019-08-27-22-47-10/11_sep/log_low/set_8/labels.old/Set_8_annotations.json"
-  output_pred_json ="/media/yl/downloads/raw_data/CETRAN_ST-cloudy-day_2019-08-27-22-47-10/11_sep/log_low/set_8/prediction.json"
+  label_json = "/media/yl/demo_ssd/raw_data/CETRAN_ST-cloudy-day_2019-08-27-22-47-10/11_sep/log_low/set_8/labels.old/Set_8_annotations.json"
+  output_pred_json ="/media/yl/demo_ssd/raw_data/CETRAN_ST-cloudy-day_2019-08-27-22-47-10/11_sep/log_low/set_8/prediction.json"
+  
+  fp_json = "/media/yl/demo_ssd/raw_data/CETRAN_ST-cloudy-day_2019-08-27-22-47-10/11_sep/log_low/set_8/fused_pose/fused_pose_new.json" 
