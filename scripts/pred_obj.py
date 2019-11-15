acf70081167be73e2fa46451a4ef303545b54117
ylc <mpechon@nus.edu.sg>
2019-11-15 08:45:37 +0800

    laptop

--- scripts/pred_obj.py
+++ scripts/pred_obj.py
@@ -27,11 +27,11 @@ class trk_pt():
       self.w= init_state["geometry"]['dimensions']['x']
       self.l= init_state["geometry"]['dimensions']['y']
     elif kf_x is not None:
-      self.x = init_state["geometry"]["position"]['x']
-      self.y= init_state["geometry"]["position"]['y']
-      self.ori= init_state["geometry"]['rotation']['z']
-      self.w= init_state["geometry"]['dimensions']['x']
-      self.l= init_state["geometry"]['dimensions']['y']
+      self.x = kf_x[0]
+      self.y= kf_x[1]
+      self.ori= kf_x[3]
+      self.w= kf_x[4]
+      self.l= kf_x[5]
     else:
       print "trk_pt init: either init_state or kf_x must be defined"
       raise InputError
@@ -50,7 +50,7 @@ class trk_pt():
                      0.0, 0.0, 0.0, 0.0])
   def get_z(self):
     return np.array([self.x, self.y, 0.0, self.ori,
-                     self.w, self.l, 1.0])
+                     self.w, self.l, 1.0]).reshape(PRED_MEAS_SIZE,1)
 
 '''
 @param init_state: the array from the labels json
@@ -68,7 +68,7 @@ class Pred_obj():
     obj_id_list.append(self.id)
     
     # KF for obj 
-    self.kf = KalmanFilter(dim_x=yl.STATE_SIZE, dim_z=yl.STATE_SIZE) 
+    self.kf = KalmanFilter(dim_x=PRED_STATE_SIZE, dim_z=PRED_MEAS_SIZE) 
     if PRED_MOTION_MOD == "CYRA":
       self.kf.F = yl.get_CYRA_F(pred_delta_t) 
       self.kf.R = R
@@ -105,7 +105,7 @@ class Pred_obj():
     if PRED_MOTION_MOD == "CYRA":
       # update KF
       delta_t = (curr_time-self.time_last_updated) * 0.05 #TODO: only for 20Hz dataset
-      self.kf.predict(F=get_CYRA_F(delta_t), Q=get_CYRA_Q(self.q_A, self.q_YR, delta_t)) 
+      self.kf.predict(F=yl.get_CYRA_F(delta_t), Q=yl.get_CYRA_Q(self.q_A, self.q_YR, delta_t)) 
       self.kf.update(tmp.get_z()) 
       self.time_last_updated = curr_time
     else:
@@ -113,42 +113,45 @@ class Pred_obj():
       raise TypeError
     pass
     
-  def predict(self, steps=3, delta_t=None, period=None): 
+  def predict(self, curr_time, steps=6, delta_t=None, period=None): 
     if PRED_MOTION_MOD == "CYRA":
-      self.predict_CYRA(steps, delta_t, period)
+      self.predict_CYRA(curr_time, steps, delta_t, period)
     else:
       print "unknown motion model:  ", PRED_MOTION_MOD
       raise TypeError
   
-  def predict_CYRA(self, steps=3, delta_t=None, period=None):
+  def predict_CYRA(self, curr_time, steps=6, delta_t=None, period=None):
     prediction = []
-    if len(self.past_traj) >= 3:
+    # print 'predictionting for obj', self.id
+    if len(self.past_traj) < 3:
       print "track too short, expected at least 3 previously known locations but only has ", \
             len(self.past_traj)
       return None
     else:
       # estimate const. acceleration
-      for i in range(1): 
+      for i in range(steps): 
         if delta_t is None and period is None:
-          
-          tmp_Q=yl.get_CYRA_Q(self.q_A, self.q_YR, pred_delta_t)
-          self.kf.Q = tmp_Q
-          print "before: ", self.kf
-          self.kf.predict(Q=tmp_Q)
-          print "after: ", self.kf.x
-          tmp = trk_pt(start_time=curr_time,init_state=new_state)
+          tmp_Q=yl.get_CYRA_Q(self.q_A, self.q_YR, pred_delta_t*(i+1))
+          tmp_F=yl.get_CYRA_F(pred_delta_t*(i+1)) 
+          # print "before: ", self.kf.x_prior
+          self.kf.predict(Q=tmp_Q,F=tmp_F)
+          # print "after: ", self.kf.x_prior #FIXME doble check if its correct
+          tmp = trk_pt(start_time=curr_time+pred_delta_t*(i+1),kf_x=self.kf.x_prior) # FIXME only works if curr time is same as last updated
+          prediction.append(tmp)
         # TODO handle else
-      
-      pass
+        # TODO handel if inverse fails
+    print prediction
     return prediction
     
   
 
 if __name__ == '__main__':
-  label_json = "/media/yl/demo_ssd/raw_data/CETRAN_ST-cloudy-day_2019-08-27-22-47-10/11_sep/log_low/set_8/labels.old/Set_8_annotations.json"
-  output_pred_json ="/media/yl/demo_ssd/raw_data/CETRAN_ST-cloudy-day_2019-08-27-22-47-10/11_sep/log_low/set_8/prediction.json"
-  
-  fp_json = "/media/yl/demo_ssd/raw_data/CETRAN_ST-cloudy-day_2019-08-27-22-47-10/11_sep/log_low/set_8/fused_pose/fused_pose_new.json"
+  #label_json = "/media/yl/demo_ssd/raw_data/CETRAN_ST-cloudy-day_2019-08-27-22-47-10/11_sep/log_low/set_8/labels.old/Set_8_annotations.json"
+  #fp_json = "/media/yl/demo_ssd/raw_data/CETRAN_ST-cloudy-day_2019-08-27-22-47-10/11_sep/log_low/set_8/fused_pose/fused_pose_new.json"
+  #output_pred_json ="/media/yl/demo_ssd/raw_data/CETRAN_ST-cloudy-day_2019-08-27-22-47-10/11_sep/log_low/set_8/prediction.json"
+  label_json='/home/yl/Downloads/raw_data/CETRAN_ST-cloudy-day_2019-08-27-22-47-10/11_sep/log_low/set_7/labels/Set_7_annotations.json'
+  output_pred_json = "/home/yl/Downloads/raw_data/CETRAN_ST-cloudy-day_2019-08-27-22-47-10/11_sep/log_high/set_7/pred_out.json"
+  fp_json = "/home/yl/Downloads/raw_data/CETRAN_ST-cloudy-day_2019-08-27-22-47-10/11_sep/log_high/set_7/fused_pose/fused_pose.json"
   
   init_state = {}
   init_state['classId']=1
--- scripts/predictor_wt_labels.py
+++ scripts/predictor_wt_labels.py
@@ -8,6 +8,7 @@ import numpy as np
 
 def get_pred_json(label_json,output_pred_json,fused_pose_json,R,P,q_YR,q_A):
   pred_obj_list=[]
+
   with open(label_json) as json_file:
     with open(fused_pose_json) as fp_json:
       labels_data = json.load(json_file, encoding="utf-8")
@@ -17,6 +18,7 @@ def get_pred_json(label_json,output_pred_json,fused_pose_json,R,P,q_YR,q_A):
         curr_timestep = labels['name']
         curr_timestep = int(curr_timestep.split('.')[0])
 
+        
         for obj in labels['annotations']:    
           obj_idx = 0
           for existing_obj_id in po.obj_id_list:
@@ -35,24 +37,32 @@ def get_pred_json(label_json,output_pred_json,fused_pose_json,R,P,q_YR,q_A):
 
         # for each track, do the prediction
         for p_o in pred_obj_list:
-          prediction = p_o.predict() # TODO: switch model types
+          prediction = p_o.predict(curr_timestep) # TODO: switch model types
           if prediction is not None:
             print prediction
 
-      for p_o in pred_obj_list:
-        print (p_o)
+      # for p_o in pred_obj_list:
+      #   print (p_o)
       
 
       
 
 
 if __name__ == '__main__':
+  '''
   label_json = "/media/yl/demo_ssd/raw_data/CETRAN_ST-cloudy-day_2019-08-27-22-47-10/11_sep/log_low/set_8/labels.old/Set_8_annotations.json"
   output_pred_json ="/media/yl/demo_ssd/raw_data/CETRAN_ST-cloudy-day_2019-08-27-22-47-10/11_sep/log_low/set_8/prediction.json"
   
   fp_json = "/media/yl/demo_ssd/raw_data/CETRAN_ST-cloudy-day_2019-08-27-22-47-10/11_sep/log_high/set_8/fused_pose/fused_pose_new.json" 
+  '''
 
-  R=np.eye(po.PRED_STATE_SIZE)
+  label_json='/home/yl/Downloads/raw_data/CETRAN_ST-cloudy-day_2019-08-27-22-47-10/11_sep/log_low/set_7/labels/Set_7_annotations.json'
+  output_pred_json = "/home/yl/Downloads/raw_data/CETRAN_ST-cloudy-day_2019-08-27-22-47-10/11_sep/log_high/set_7/pred_out.json"
+  fp_json = "/home/yl/Downloads/raw_data/CETRAN_ST-cloudy-day_2019-08-27-22-47-10/11_sep/log_high/set_7/fused_pose/fused_pose.json"
+
+
+
+  R=np.eye(po.PRED_MEAS_SIZE)
   P=np.eye(po.PRED_STATE_SIZE)
   po.pred_delta_t=0.5
   q_YR=1.
